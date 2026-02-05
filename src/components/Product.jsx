53f3e8fb9a62424d8c78dc2c3859a2c20899b304
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-02-05 10:22:19 +0100

    feat: wrap Product component in Link for navigation to product detail page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router";
+
 export default function Product({ product }) {
   return (
-    <article className="product-card">
-      <img src={product.image} className="product-image" />
-      <div className="product-info">
-        <h2 className="product-title">{product.title}</h2>
-        <p className="product-description">{product.description}</p>
-        <p className="product-price">DKK {product.price}</p>
-        <span className={`product-stock  ${product.inStock ? "in-stock" : "out-of-stock"}`}>
-          {product.inStock ? "På lager" : "Udsolgt"}
-        </span>
-      </div>
-    </article>
+    <Link to="/products/detail" className="product-card-link">
+      <article className="product-card">
+        <img src={product.image} className="product-image" />
+        <div className="product-info">
+          <h2 className="product-title">{product.title}</h2>
+          <p className="product-description">{product.description}</p>
+          <p className="product-price">DKK {product.price}</p>
+          <span className={`product-stock  ${product.inStock ? "in-stock" : "out-of-stock"}`}>
+            {product.inStock ? "På lager" : "Udsolgt"}
+          </span>
+        </div>
+      </article>
+    </Link>
   );
 }
